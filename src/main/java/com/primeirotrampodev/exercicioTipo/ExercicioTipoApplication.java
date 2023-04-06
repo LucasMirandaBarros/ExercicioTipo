89c0eb25b6644949799979bfbb944f6914964591
LucasMiranda <lucaswespras@gmail.com>
2023-04-05 22:05:21 -0300

    exerciciotipo

---
+++ src/main/java/com/primeirotrampodev/exercicioTipo/ExercicioTipoApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.exercicioTipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioTipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioTipoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/exercicioTipo/ExercicioTipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.exercicioTipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioTipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
